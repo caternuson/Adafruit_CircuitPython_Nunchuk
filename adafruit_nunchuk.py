f609b30788fdd7de5a4353763916e3b4fd114799
caternuson <caternuson@gmail.com>
2021-01-19 18:49:42 -0800

    change read for speed

--- adafruit_nunchuk.py
+++ adafruit_nunchuk.py
@@ -43,6 +43,7 @@ Implementation Notes
 * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 """
 import time
+from collections import namedtuple
 from adafruit_bus_device.i2c_device import I2CDevice
 
 __version__ = "0.0.0-auto.0"
@@ -68,41 +69,45 @@ class Nunchuk:
             i2c_dev.write(b"\xFB\x00")
 
     @property
-    def joystick(self):
-        """Return tuple of current joystick position."""
+    def values(self):
+        """All values as a named tuple."""
         self._read_data()
-        return self.buffer[0], self.buffer[1]
+        return namedtuple("Values", ("jx", "jy", "C", "Z", "ax", "ay", "az"))(
+            self.buffer[0],
+            self.buffer[1],
+            not bool(self.buffer[5] & 0x02),
+            not bool(self.buffer[5] & 0x01),
+            ((self.buffer[5] & 0xC0) >> 6) | (self.buffer[2] << 2),
+            ((self.buffer[5] & 0x30) >> 4) | (self.buffer[3] << 2),
+            ((self.buffer[5] & 0x0C) >> 2) | (self.buffer[4] << 2)
+        )
+
+    @property
+    def joystick(self):
+        """The joystick position."""
+        v = self.values
+        return v.jx, v.jy
 
     @property
     def button_C(self):  # pylint: disable=invalid-name
-        """Return current pressed state of button C."""
-        return not bool(self._read_data()[5] & 0x02)
+        """The pressed state of button C."""
+        return self.values.C
 
     @property
     def button_Z(self):  # pylint: disable=invalid-name
-        """Return current pressed state of button Z."""
-        return not bool(self._read_data()[5] & 0x01)
+        """The pressed state of button Z."""
+        return self.values.Z
 
     @property
     def acceleration(self):
-        """Return 3 tuple of accelerometer reading."""
-        self._read_data()
-        x = (self.buffer[5] & 0xC0) >> 6
-        x |= self.buffer[2] << 2
-        y = (self.buffer[5] & 0x30) >> 4
-        y |= self.buffer[3] << 2
-        z = (self.buffer[5] & 0x0C) >> 2
-        z |= self.buffer[4] << 2
-        return x, y, z
+        """The acceleration as a 3 tuple."""
+        v = self.values
+        return v.ax, v.ay, v.az
 
     def _read_data(self):
-        return self._read_register(b"\x00")
-
-    def _read_register(self, address):
+        # read all of the current data into local buffer via a single (slow) i2c xfer
         with self.i2c_device as i2c:
             time.sleep(_I2C_READ_DELAY)
-            i2c.write(address)
+            i2c.write(b"\x00")
             time.sleep(_I2C_READ_DELAY)
             i2c.readinto(self.buffer)
-        time.sleep(_I2C_READ_DELAY)
-        return self.buffer
--- examples/nunchuk_analog_mouse.py
+++ examples/nunchuk_analog_mouse.py
@@ -15,45 +15,36 @@ scaleY = 0.3
 cDown = False
 zDown = False
 
-# This is to allow double checking (only on left click - and it doesn't really work)
-CHECK_COUNT = 0
-
-
-# This is just to show that we're getting back data - uncomment it and hold down the buttons
-# while True:
-#    print((0 if nc.button_C else 1, 0 if nc.button_Z else 1))
-
 while True:
-    x, y = nc.joystick
-    # Eliminate spurious reads
+    # get current nunchuk values
+    values = nc.values
+
+    # the ones we use here
+    x = values.jx
+    y = values.jy
+    c = values.C
+    z = values.Z
+
+    # skip spurious reads
     if x == 255 or y == 255:
         continue
+
+    # add offsets
     relX = x - centerX
     relY = centerY - y
 
+    # move cursor
     m.move(int(scaleX * relX), int(scaleY * relY), 0)
 
-    c = nc.button_C
-    z = nc.button_Z
-
+    # left click
     if z and not zDown:
-        stillDown = True
-        for n in range(CHECK_COUNT):
-            if nc.button_Z:
-                stillDown = False
-                break
-        if stillDown:
-            m.press(Mouse.LEFT_BUTTON)
-            zDown = True
+        m.press(Mouse.LEFT_BUTTON)
+        zDown = True
     elif not z and zDown:
-        stillDown = True
-        for n in range(CHECK_COUNT):
-            if not nc.button_Z:
-                stillDown = False
-                break
-        if stillDown:
-            m.release(Mouse.LEFT_BUTTON)
-            zDown = False
+        m.release(Mouse.LEFT_BUTTON)
+        zDown = False
+
+    # right click
     if c and not cDown:
         m.press(Mouse.RIGHT_BUTTON)
         cDown = True
